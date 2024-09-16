3958e40bfc003826f929a2c46662b4f962cf054f
Jhay-Great <alemonai.jenef@gmail.com>
2024-09-16 07:33:03 +0000

    adds styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -16,7 +16,4 @@ export const routes: Routes = [
   },
 ];
 
-// {
-//     path: 'lazy',
-//     loadComponent: () => import('./lazy-component').then(m => m.LazyComponent)
-//   }
+
--- src/app/components/display-all-posts/display-all-posts.component.ts
+++ src/app/components/display-all-posts/display-all-posts.component.ts
@@ -21,11 +21,8 @@ import { Router } from '@angular/router';
 export class DisplayAllPostsComponent implements OnInit {
 
   data!:Observable<IPost[]>;
-  // currentPage!:Observable<number>;
-  // totalPages!:Observable<number>;
 
   constructor (
-    // private postService: PostService,
     private store: Store<AppState>,
     private router: Router,
   ) { }
@@ -33,24 +30,12 @@ export class DisplayAllPostsComponent implements OnInit {
   ngOnInit(): void {
 
     this.data = this.store.select(selectAllPost);
-    // this.currentPage = this.store.select(selectCurrentPage);
-    // this.totalPages = this.store.select(selectTotalPages);
-
-    
-    
   }
 
   routeTo (id:number) {
     console.log(id);
     this.router.navigate([`post/:${id}`])
   }
-
-  // next () {
-  //   this.store.dispatch(nextPage());
-  // }
-
-  // previous () {
-  //   this.store.dispatch(previousPage());
-  // }
+  
 
 }
